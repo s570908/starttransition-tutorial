92d3b84a3ecd090a8d844dfedf3d340c35308e3c
s570908 <songohyoung@gmail.com>
2023-08-27 02:07:03 +0900

    Updating the parent component in a transition

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
-import { useState, useTransition } from 'react';
-import TabButton from './TabButton.js';
-import AboutTab from './AboutTab.js';
-import PostsTab from './PostsTab.js';
-import ContactTab from './ContactTab.js';
+import { useState } from "react";
+import TabButton from "./TabButton.js";
+import AboutTab from "./AboutTab.js";
+import PostsTab from "./PostsTab.js";
+import ContactTab from "./ContactTab.js";
 
 export default function TabContainer() {
-  const [isPending, startTransition] = useTransition();
-  const [tab, setTab] = useState('about');
-
-  function selectTab(nextTab) {
-    startTransition(() => {
-      setTab(nextTab);      
-    });
-  }
-
+  const [tab, setTab] = useState("about");
   return (
     <>
-      <TabButton
-        isActive={tab === 'about'}
-        onClick={() => selectTab('about')}
-      >
+      <TabButton isActive={tab === "about"} onClick={() => setTab("about")}>
         About
       </TabButton>
-      <TabButton
-        isActive={tab === 'posts'}
-        onClick={() => selectTab('posts')}
-      >
+      <TabButton isActive={tab === "posts"} onClick={() => setTab("posts")}>
         Posts (slow)
       </TabButton>
-      <TabButton
-        isActive={tab === 'contact'}
-        onClick={() => selectTab('contact')}
-      >
+      <TabButton isActive={tab === "contact"} onClick={() => setTab("contact")}>
         Contact
       </TabButton>
       <hr />
-      {tab === 'about' && <AboutTab />}
-      {tab === 'posts' && <PostsTab />}
-      {tab === 'contact' && <ContactTab />}
+      {tab === "about" && <AboutTab />}
+      {tab === "posts" && <PostsTab />}
+      {tab === "contact" && <ContactTab />}
     </>
   );
 }
--- src/TabButton.js
+++ src/TabButton.js
@@ -1,15 +1,21 @@
-import { useTransition } from 'react';
+import { useTransition } from "react";
 
 export default function TabButton({ children, isActive, onClick }) {
+  const [isPending, startTransition] = useTransition();
   if (isActive) {
-    return <b>{children}</b>
+    return <b>{children}</b>;
   }
   return (
-    <button onClick={() => {
-      onClick();
-    }}>
+    <button
+      onClick={() => {
+        // onClick()은 parent component, 즉 TabContainer의 state를 update한다. 이 state update는 transition으로 marked된다.
+        // 이전의 컴포넌트 렌더링은 다음 컴포넌트 렌더링에 의해서 transition, non-blocking, interruptible 하게 된다.
+        startTransition(() => {
+          onClick();
+        });
+      }}
+    >
       {children}
     </button>
-  )
+  );
 }
-
