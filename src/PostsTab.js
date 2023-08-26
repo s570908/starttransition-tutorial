62858e2638395440edbf5f50f359f5967849b018
s570908 <songohyoung@gmail.com>
2023-08-27 03:07:32 +0900

    Unwanted loading indicators: Hiding the entire tab container to show a loading indicator leads to a jarring user experience.

--- src/PostsTab.js
+++ src/PostsTab.js
@@ -1,31 +1,50 @@
-import { memo } from 'react';
+import { fetchData } from "./data.js";
 
-const PostsTab = memo(function PostsTab() {
-  // Log once. The actual slowdown is inside SlowPost.
-  console.log('[ARTIFICIALLY SLOW] Rendering 500 <SlowPost />');
+// Note: this component is written using an experimental API
+// that's not yet available in stable versions of React.
 
-  let items = [];
-  for (let i = 0; i < 500; i++) {
-    items.push(<SlowPost key={i} index={i} />);
-  }
+// For a realistic example you can follow today, try a framework
+// that's integrated with Suspense, like Relay or Next.js.
+
+function PostsTab() {
+  const posts = use(fetchData("/posts"));
   return (
     <ul className="items">
-      {items}
+      {posts.map((post) => (
+        <Post key={post.id} title={post.title} />
+      ))}
     </ul>
-  );
-});
-
-function SlowPost({ index }) {
-  let startTime = performance.now();
-  while (performance.now() - startTime < 1) {
-    // Do nothing for 1 ms per item to emulate extremely slow code
-  }
-
-  return (
-    <li className="item">
-      Post #{index + 1}
-    </li>
   );
 }
 
+function Post({ title }) {
+  console.log(`Post ${title}: rendered`);
+  return <li className="item">{title}</li>;
+}
+
 export default PostsTab;
+
+// This is a workaround for a bug to get the demo running.
+// TODO: replace with real implementation when the bug is fixed.
+function use(promise) {
+  if (promise.status === "fulfilled") {
+    return promise.value;
+  } else if (promise.status === "rejected") {
+    throw promise.reason;
+  } else if (promise.status === "pending") {
+    throw promise;
+  } else {
+    promise.status = "pending";
+    promise.then(
+      (result) => {
+        promise.status = "fulfilled";
+        promise.value = result;
+      },
+      (reason) => {
+        promise.status = "rejected";
+        promise.reason = reason;
+      }
+    );
+    throw promise;
+  }
+}
